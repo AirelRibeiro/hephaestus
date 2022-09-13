aa85651d1853b832cf8b03a5af044ad2a15cfb35
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 16:43:36 -0300

    feat: creates ProductsModel

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,3 +1,9 @@
 import { RowDataPacket } from 'mysql2/promise';
 import connection from './connection';
 import Product from '../interfaces/product.inteface';
+
+const ProductsModel = {
+
+};
+
+export default ProductsModel;
