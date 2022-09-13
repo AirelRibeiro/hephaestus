4d000b030e3a0528f8a7986515db23a8ff148c8f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 18:09:11 -0300

    feat: creates create function in ProductsModel to insert product in database

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -5,6 +5,12 @@ import Product from '../interfaces/product.inteface';
 interface ProductWithRow extends Product, RowDataPacket{}
 
 const ProductsModel = {
+  create: async ({ name, amount }: Product): Promise<Product> => {
+    const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?);';
+    const [{ insertId }] = await connection.execute<ResultSetHeader>(query, [name, amount]);
+    return { id: insertId, name, amount };
+  },
+
   getAll: async (): Promise<Product[]> => {
     const [result] = await connection
       .execute<ProductWithRow[]>('SELECT * FROM Trybesmith.Products;');
