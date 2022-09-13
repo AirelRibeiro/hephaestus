c58639619c5696fe7cc21083621d2d3f339da440
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 18:28:36 -0300

    feat: creates create function to insert user in database

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -2,8 +2,16 @@ import { ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
 import User from '../interfaces/user.interface';
 
+// interface UserWithRow extends User, RowDataPacket{}
 
 const UsersModel = {
+  create: async ({ username, classe, level, password }: User): Promise<number> => {
+    const query = `INSERT INTO Trybesmith.Users (username, classe, level, password)
+      VALUES (?, ?, ?, ?);`;
+    const [{ insertId }] = await connection
+      .execute<ResultSetHeader>(query, [username, classe, level, password]);
+    return insertId;
+  },
 };
 
 export default UsersModel;
