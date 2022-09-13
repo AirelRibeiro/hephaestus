b8cfb166060effe0a7ca981f57ba0a58a61d1fe9
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 20:07:23 -0300

    feat: creates getAll function to list all orders

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -2,6 +2,10 @@ import { Request, Response } from 'express';
 import OrdersService from '../services/orders.service';
 
 const OrdersController = {
+  getAll: async (_req: Request, res: Response): Promise<Response> => {
+    const orders = await OrdersService.getAll();
+    return res.status(200).json(orders);
+  },
 };
 
 export default OrdersController;
