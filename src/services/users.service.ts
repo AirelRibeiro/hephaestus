3e108baf1a7c3988fa1b63e1ffa2b60a76657178
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 18:34:05 -0300

    feat: creates create function to insert Users in database

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -4,5 +4,12 @@ import User from '../interfaces/user.interface';
 import UsersModel from '../models/users.model';
 
 const UsersService = {
+  create: async ({ username, classe, level, password }: User): Promise<JWTToken> => {
+    const id = await UsersModel.create({ username, classe, level, password });
+    const token = jwt
+      .sign({ data: { username, id } }, 'JWT_SECRET', { algorithm: 'HS256' });
+    return { token };
+  },
+};
 
 export default UsersService;
