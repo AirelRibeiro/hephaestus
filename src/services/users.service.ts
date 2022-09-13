990c8227635d7041d1d08fde7d8d62cee0b7999f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 18:33:35 -0300

    feat: creates UsersService

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -2,3 +2,7 @@ import jwt from 'jsonwebtoken';
 import JWTToken from '../interfaces/token.interface';
 import User from '../interfaces/user.interface';
 import UsersModel from '../models/users.model';
+
+const UsersService = {
+
+export default UsersService;
