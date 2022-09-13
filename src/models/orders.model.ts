5defc44f8a2fb5f320da9b82c1cbe78be70e0485
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 19:59:55 -0300

    feat: creates interface with Order and RowDataPacket

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -1,3 +1,6 @@
 import { RowDataPacket } from 'mysql2/promise';
 import Order from '../interfaces/order.interface';
 import connection from './connection';
+
+interface OrderWithRow extends Order, RowDataPacket{}
+
