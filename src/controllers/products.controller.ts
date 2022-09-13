b123bbc7af2cb670c7ee6d601b60a4516d670cdb
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 18:10:26 -0300

    feat: creates creat function in ProductsController to insert productsin database

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -2,6 +2,11 @@ import { Request, Response } from 'express';
 import ProductsService from '../services/products.service';
 
 const ProductsController = {
+  create: async (req: Request, res: Response): Promise<Response> => {
+    const product = await ProductsService.create(req.body);
+    return res.status(201).json(product);
+  },
+
   getAll: async (_req: Request, res: Response): Promise<Response> => {
     const products = await ProductsService.getAll();
     return res.status(200).json(products);
