66816788fcbc4485fa6e5d78372de677b16d1d65
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 18:07:51 -0300

    feat: creates create function to insert products in database

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -2,6 +2,11 @@ import Product from '../interfaces/product.inteface';
 import ProductsModel from '../models/products.model';
 
 const ProductsService = {
+  create: async ({ name, amount }: Product): Promise<Product> => {
+    const product = await ProductsModel.create({ name, amount });
+    return product;
+  },
+
   getAll: async (): Promise<Product[]> => {
     const products = await ProductsModel.getAll();
     return products;
