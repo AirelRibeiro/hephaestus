f5000e1ef1a855a11c57e0dfb0ee456498c71a40
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 16:53:01 -0300

    feat: creates getAll for list all products

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -5,6 +5,11 @@ import Product from '../interfaces/product.inteface';
 interface ProductWithRow extends Product, RowDataPacket{}
 
 const ProductsModel = {
+  getAll: async (): Promise<Product[]> => {
+    const [result] = await connection
+      .execute<ProductWithRow[]>('SELECT * FROM Trybesmith.Products;');
+    return result;
+  },
 
 };
 
