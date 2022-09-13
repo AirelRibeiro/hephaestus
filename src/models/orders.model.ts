d6377ec82ad21670db146b93e1d7666f7b400863
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 20:01:51 -0300

    feat: creates getAll function to list all orders

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -5,6 +5,19 @@ import connection from './connection';
 interface OrderWithRow extends Order, RowDataPacket{}
 
 const OrdersModel = {
+  getAll: async (): Promise<Order[]> => {
+    const query = `
+    SELECT orders.id AS id, orders.userId AS userId, JSON_ARRAYAGG(product.id) AS productsIds
+    FROM  Trybesmith.Orders AS orders
+    INNER JOIN Trybesmith.Products AS product
+    ON orders.id = product.orderId
+    GROUP BY orders.id
+    ORDER BY orders.userId ASC;
+    `;
+    const [result] = await connection
+      .execute<OrderWithRow[]>(query);
+    return result;
+  },
 };
 
 export default OrdersModel;
