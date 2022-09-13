1cc64780182f0201656a474b450278e4d7ecfb50
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 18:32:17 -0300

    feat: imports jwt, JWTToken, User interface and UsersModel

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,6 +2,10 @@ import { Request, Response } from 'express';
 import UsersService from '../services/users.service';
 
 const UsersController = {
+  create: async (req: Request, res: Response): Promise<Response> => {
+    const token = await UsersService.create(req.body);
+    return res.status(201).json(token);
+  },
 };
 
 export default UsersController;
---
+++ src/services/users.service.ts
@@ -0,0 +1,4 @@
+import jwt from 'jsonwebtoken';
+import JWTToken from '../interfaces/token.interface';
+import User from '../interfaces/user.interface';
+import UsersModel from '../models/users.model';
