989471462d11cd732da8c73655de8ff2c16bc5a1
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 17:04:07 -0300

    feat: creates getAll to list all products

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,2 +1,11 @@
 import { Request, Response } from 'express';
 import ProductsService from '../services/products.service';
+
+const ProductsController = {
+  getAll: async (_req: Request, res: Response): Promise<Response> => {
+    const products = await ProductsService.getAll();
+    return res.status(200).json(products);
+  },
+};
+
+export default ProductsController;
