7a329e68d38dcf7049b51a0a07c769a2c45fc657
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 20:03:15 -0300

    feat: creates OrdersService and getAll function to list all orders

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,3 +1,11 @@
 import Order from '../interfaces/order.interface';
 import OrdersModel from '../models/orders.model';
 
+const OrdersService = {
+  getAll: async (): Promise<Order[]> => {
+    const orders = await OrdersModel.getAll();
+    return orders;
+  },
+};
+
+export default OrdersService;
