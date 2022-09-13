b234bcc1ab79cdf7009ed967ac1aeaab8f8d1deb
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 16:50:27 -0300

    feat: creates interface for Product with RowDataPacket

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -2,6 +2,8 @@ import { RowDataPacket } from 'mysql2/promise';
 import connection from './connection';
 import Product from '../interfaces/product.inteface';
 
+interface ProductWithRow extends Product, RowDataPacket{}
+
 const ProductsModel = {
 
 };
