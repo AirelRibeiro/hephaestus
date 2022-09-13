624dab8c348cd970eb0aa4de88fca2731901a4c4
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 17:05:17 -0300

    feat: includes productsRoute in app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import productsRoute from './routes/products.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productsRoute);
 
 export default app;
