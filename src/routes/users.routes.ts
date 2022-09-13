905229feaaa891ba03e2f110e1fe0076ffae5694
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 18:07:04 -0300

    feat: creates usersRoute and route to include user in database

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,2 +1,8 @@
 import express from 'express';
 import UsersController from '../controllers/users.controller';
+
+const usersRoute = express.Router();
+
+usersRoute.post('/', UsersController.create);
+
+export default usersRoute;
