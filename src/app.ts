85299ae66b51be6442fb234a887df3d6efaef2c2
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 21:46:25 -0300

    feat: includes route for login app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import UsersController from './controllers/users.controller';
+import validLoginFields from './middlewares/loginValidation';
 import ordersRoute from './routes/orders.routes';
 import productsRoute from './routes/products.routes';
 import usersRoute from './routes/users.routes';
@@ -10,5 +12,6 @@ app.use(express.json());
 app.use('/products', productsRoute);
 app.use('/users', usersRoute);
 app.use('/orders', ordersRoute);
+app.post('/login', validLoginFields, UsersController.login);
 
 export default app;
