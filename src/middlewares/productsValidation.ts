97eb6f66a8aa67f73c001893d245d2e6d48a5970
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-14 16:13:55 -0300

    feat: creates validation schema to name and amount fields

--- src/middlewares/productsValidation.ts
+++ src/middlewares/productsValidation.ts
@@ -1,3 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
 
+const fieldsValidations = Joi.object({
+  name: Joi.string().required().min(3).messages({
+    'any.required': '"name" is required|400',
+    'string.base': '"name" must be a string|422',
+    'string.min': '"name" length must be at least 3 characters long|422',
+  }),
+  amount: Joi.string().required().min(3).messages({
+    'any.required': '"amount" is required|400',
+    'string.base': '"amount" must be a string|422',
+    'string.min': '"amount" length must be at least 3 characters long|422',
+  }),
+});
+
