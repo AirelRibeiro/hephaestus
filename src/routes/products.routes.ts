e4b60dc6ccf8482d39fcdc82b0d034c2df3d3a82
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-14 16:19:32 -0300

    feat: includes validation for insert fields  a product

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import ProductsController from '../controllers/products.controller';
+import validProductFields from '../middlewares/productsValidation';
 
 const productsRoute = express.Router();
 
 productsRoute.get('/', ProductsController.getAll);
-productsRoute.post('/', ProductsController.create);
+productsRoute.post('/', validProductFields, ProductsController.create);
 
 export default productsRoute;
