be76cf450e899d66268453976b9a76e43918e796
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-13 21:44:34 -0300

    feat: creates login function in UsersController

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -6,6 +6,13 @@ const UsersController = {
     const token = await UsersService.create(req.body);
     return res.status(201).json(token);
   },
+
+  login: async (req: Request, res: Response): Promise<Response> => {
+    const { username, password } = req.body;
+    const token = await UsersService.login(username, password);
+    if (typeof token !== 'string') return res.status(200).json(token);
+    return res.status(401).json({ message: token });
+  },
 };
 
 export default UsersController;
