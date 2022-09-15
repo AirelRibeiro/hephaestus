99c5956b9c6129e2dab8892a3d0780d444c3bc20
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-14 21:43:04 -0300

    feat: creates create function in OrdersService

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -6,6 +6,11 @@ const OrdersService = {
     const orders = await OrdersModel.getAll();
     return orders;
   },
+
+  create: async (userId: number, productsIds: number[]) => {
+    const insert = await OrdersModel.create(userId, productsIds);
+    return insert;
+  },
 };
 
 export default OrdersService;
