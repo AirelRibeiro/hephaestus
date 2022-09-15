d1927a42539dd5e6e3f71ba9e12a42499cd4295f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-14 21:48:17 -0300

    feat: creates create function in OrdersController

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from 'express';
+import DataToken from '../interfaces/dataToken.interface';
 import OrdersService from '../services/orders.service';
+
+interface ReqWithUser extends Request {
+  user: DataToken
+}
 
 const OrdersController = {
   getAll: async (_req: Request, res: Response): Promise<Response> => {
     const orders = await OrdersService.getAll();
     return res.status(200).json(orders);
+  },
+
+  create: async (req: Request, res: Response): Promise<Response> => {
+    const userId = (req as ReqWithUser).user.data.id;
+    const { productsIds } = req.body;
+    return OrdersService.create(userId, productsIds)
+      .then(() => res.status(201).json({ userId, productsIds }));
   },
 };
 
