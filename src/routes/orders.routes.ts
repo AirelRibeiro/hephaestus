d7cea1cb582d9ad4ffb253b4c1cbf38a7b3e8da5
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-14 21:44:27 -0300

    feat: includes route to insert order in database

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import OrdersController from '../controllers/orders.controller';
+import validOrderInsertion from '../middlewares/ordersValidation';
 
 const ordersRoute = express.Router();
 
 ordersRoute.get('/', OrdersController.getAll);
+ordersRoute.post('/', validOrderInsertion, OrdersController.create);
 
 export default ordersRoute;
