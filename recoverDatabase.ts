c76a4d3b661881943bc2b2e833751b46c14b0b81
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-26 16:33:40 -0300

    feat: includes file with function to recover database

--- recoverDatabase.ts
+++ recoverDatabase.ts
@@ -6,9 +6,9 @@ import connection from './src/models/connection';
 
 export default async function recreateDatabase(conn: Pool) {
   try {
-    const importPath = path.resolve(__dirname, 'Trybesmith.sql');
-    const seedDBContent = fs.readFileSync(importPath).toString();
-    const queries = seedDBContent.split(';').filter((p) => p.trim());
+    const filePath = path.resolve(__dirname, 'Trybesmith.sql');
+    const dbContent = fs.readFileSync(filePath).toString();
+    const queries = dbContent.split(';').filter((p) => p.trim());
     for (let i = 0; i < queries.length; i += 1) { 
       const query = queries[i];
       // eslint-disable-next-line no-await-in-loop
@@ -19,11 +19,9 @@ export default async function recreateDatabase(conn: Pool) {
   }
 }
 
-if (require.main === module) {
-  recreateDatabase(connection)
-    .then(async () => {
-      console.log('Banco Restaurado com sucesso');
-      await connection.end();
-      process.exit(0);
-    });
-}
+recreateDatabase(connection)
+  .then(async () => {
+    console.log('Banco Restaurado com sucesso');
+    await connection.end();
+    process.exit(0);
+  });
